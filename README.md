b7f8db6fa883638e1058f8b4553c38b845eb21b4
Irving <irving.zhao.dev@hotmail.com>
2017-05-22 14:52:32 +0800

    VUE+webpack 配置

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # webpack-vue-demo
 Webpack2+VUE单文件异步加载实例，辅助scss、ES6 
+
+- src 源码目录
+- dist 打包目录
+- src/index.js 首页脚本
+- src/pages.js 页面信息配置脚本
+- src/index.html 首页页面
+- src/app.vue 测试异步加载模块
+
+VUE 组件中的渲染节点查找默认为全页面查找，使用时需注意
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require("path");// 路径工具
+const ExtractTextPlugin = require("extract-text-webpack-plugin"); // .vue 文件中的文本提取插件
+const CopyWebpackPlugin = require("copy-webpack-plugin");//文件拷贝插件
+
+
+module.exports = {
+    entry: {
+        index: "./src/index.js"//首页入口
+    },
+
+    //输出配置
+    output: {
+        path: path.resolve(__dirname, 'dist/'),//输出目录  __dirname 为 项目当前目录
+        filename: '[name].js?[chunkhash]', // 输出文件名  name代表  entry中的 name值
+        chunkFilename: 'chunk/[name].js?[chunkhash]',// 模块的文件打包名以及加载请求地址，chunkhash 文件hash
+        publicPath: "/dist/" //动态加载文件时的上下文，会添加到文件地址前，可根据需求具体修改
+    },
+
+    //模块配置
+    module: {
+        //文件加载器配置
+        rules: [
+            {
+                test: /\.vue$/,//vue 加载器
+                loader: 'vue-loader',
+                options: {
+                    // .vue 文件中的样式 脚本 加载器配置
+                    loaders: {
+                        scss: 'vue-style-loader!css-loader!sass-loader!autoprefixer-loader', // <style lang="scss">
+                        sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax', // <style lang="sass">
+                        es6: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['env'],
+                                plugins: ['transform-runtime']
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                //js es6 转换
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            }
+            // ,
+            // {
+            //     //html 加载
+            //     test: /\.html$/,
+            //     use: {
+            //         loader: "html-loader"
+            //     }
+            // },
+            // {
+            //     //css 加载器
+            //     test: /\.css$/,
+            //     exclude: /(themes)/,
+            //     loader: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
+            // }
+        ]
+    },
+    resolve: {
+        alias: {
+            // require中 使用的  别名
+            Components: path.resolve(__dirname, "src/components/"),
+            Pages: path.resolve(__dirname, "src/pages"),
+            Themes: path.resolve(__dirname, "src/themes")
+        },
+        // require中 自动追加的后缀名  * 用来 全名时匹配，替代 webpack 1.0 中的 空字符串
+        extensions: ["*", ".js", ".vue"]
+    },
+    plugins: [
+        //css 加载
+        // new ExtractTextPlugin("css/[name].css"),
+        // new TransferWebpackPlugin([{from: "src/plugin", to: "plugin"}, {from: "src/theme", to: "theme"}]),
+        //文件拷贝
+        new CopyWebpackPlugin([
+            // {from: "src/plugin", to: "plugin"},
+            // {from: "src/theme", to: "theme"},
+            {from: "src/index.html", to: "index.html"}
+        ])
+    ]
+};
